0c98d5816c5c91b9a975d4596150d177689ee7d7
Akkuman <akkumans@qq.com>
2018-08-19 23:24:56 +0800

    bug修正,在serve.go45行处静态文件目录写死,委曲求全

---
+++ bundle/js/main.js
@@ -0,0 +1,133 @@
+//时间戳转时间
+$('time').each(function() {
+    var T = new Date(parseInt($(this).attr('datetime'))*1000);
+    //timetextlist=[Sun, Mar, 01, 2015]
+    var timetextlist = T.toDateString().split(' ');
+    var timetext = timetextlist[1] + ' ' + timetextlist[2] + ', ' + timetextlist[3];
+    $(this).html(timetext);
+});
+
+var searchTpl = '<li class="post-item grid-item"><a class="post-link" href="{{link}}">{{title}}<section class="post-content">{{preview}}</section></a></li>'
+
+// pick from underscore
+var debounce = function(func, wait, immediate) {
+  var timeout, args, context, timestamp, result;
+
+  var later = function() {
+    var last = Date.now() - timestamp;
+
+    if (last < wait && last >= 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      if (!immediate) {
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+    }
+  };
+
+  return function() {
+    context = this;
+    args = arguments;
+    timestamp = Date.now();
+    var callNow = immediate && !timeout;
+    if (!timeout) timeout = setTimeout(later, wait);
+    if (callNow) {
+      result = func.apply(context, args);
+      context = args = null;
+    }
+
+    return result;
+  };
+}
+
+var initSearch = function() {
+  var searchDom = $('#menu-search')
+  if (!searchDom.length) return
+  var searchWorker = new Worker(root + '/bundle/js/searchWorker.js')
+  var oriHtml = $('.post-list').html()
+  var workerStarted = false
+  var tpl = function(keywords, title, preview, link, cover) {
+    for (var i = 0; i < keywords.length; i++) {
+      var keyword = keywords[i]
+      var wrap = '<span class="searched">' + keyword + '</span>'
+      var reg = new RegExp(keyword, 'ig')
+      title = title.replace(reg, wrap)
+      preview = preview.replace(reg, wrap)
+    }
+    return searchTpl
+    .replace('{{title}}', title)
+    .replace('{{link}}', link + '?search=' + keywords)  // append keywords to url
+    .replace('{{preview}}', preview)
+  }
+  searchWorker.onmessage = function(event) {
+    var results = event.data.results
+    var keywords = event.data.keywords
+    if (results.length) {
+      var retHtml = ''
+      for (var i = 0; i < results.length; i++) {
+        var item = results[i]
+        var itemHtml = tpl(keywords, item.title, item.preview, item.link, item.cover)
+        retHtml += itemHtml
+      }
+      $('.nav-page').hide()
+      $('.post-list').html(retHtml)
+    } else {
+      var keyword = event.data.keyword
+      if (keyword) {
+        $('.nav-page').hide()
+        $('.post-list').html('<div class="empty">未搜索到 "<span>' + keyword + '</span>"</div>')
+      } else {
+        $('.nav-page').show()
+        $('.post-list').html(oriHtml)
+      }
+    }
+  }
+  searchDom.on('input', debounce(function() {
+    var keyword = $(this).val().trim()
+    if (keyword) {
+      searchWorker.postMessage({
+        search: 'search',
+        keyword: keyword
+      })
+    } else {
+      $('.nav-page').show()
+      $('.post-list').html(oriHtml)
+    }
+  }, 500))
+  searchDom.on('focus', function() {
+    if (!workerStarted) {
+      searchWorker.postMessage({
+        action: 'start',
+        root: root
+      })
+      workerStarted = true
+    }
+  })
+}
+
+$(function() {
+  // append image description
+  /*
+  $('img').each(function(idx, item) {
+    $item = $(item)
+    if ($item.attr('data-src')) {
+      $item.wrap('<a href="' + $item.attr('data-src') + '" target="_blank"></a>')
+      var imageAlt = $item.prop('alt')
+      if ($.trim(imageAlt)) $item.parent('a').after('<div class="image-alt">' + imageAlt + '</div>')
+    }
+  })
+  */
+  // lazy load images
+  if ($('img').unveil) {
+    $('img').unveil(200, function() {
+      $(this).load(function() {
+        this.style.opacity = 1
+      })
+    })
+  }
+  //init search
+  initSearch()
+})
+
